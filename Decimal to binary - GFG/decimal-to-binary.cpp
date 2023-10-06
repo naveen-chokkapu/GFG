5d5481ad98a8cbe75bea3c4ce0330de1f589fe8f
NAVEEN CHOKKAPU <91678670+naveen-chokkapu@users.noreply.github.com>
2023-10-06 20:15:04 +0530

    Added solution - LeetHub

---
+++ Decimal to binary - GFG/decimal-to-binary.cpp
@@ -0,0 +1,41 @@
+//{ Driver Code Starts
+//Initial Template for C++
+
+#include <bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+//User function Template for C++
+
+void toBinary(int N)
+{
+    string s;
+    while(N) {
+        s+=N%2+'0';
+        N/=2;
+    }
+    reverse(s.begin(),s.end());
+    cout<<s;
+        
+}
+
+//{ Driver Code Starts.
+
+int main() {
+	//code
+	
+	int t;
+	cin>>t;
+	
+	
+	while(t--)
+	{
+	    int n;
+	    cin>>n;
+	    
+	    toBinary(n);
+	    cout<<endl;
+	}
+	return 0;
+}
+// } Driver Code Ends
